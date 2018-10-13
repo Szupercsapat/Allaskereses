d1ef22359c2a4aa568e43b1de1370ca06662363c
DESKTOP-LKK7QLH\Bonfette <nelzonka@gmail.com>
2018-10-13 15:45:54 +0200

    Próba komment

--- BACKEND/Szupercsapat-RFT/src/main/java/com/bharat/spring/data/SzupercsapatRftApplication.java
+++ BACKEND/Szupercsapat-RFT/src/main/java/com/bharat/spring/data/SzupercsapatRftApplication.java
@@ -8,5 +8,7 @@ public class SzupercsapatRftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SzupercsapatRftApplication.class, args);
+
+	//asdasdasd proba komment
 	}
 }
